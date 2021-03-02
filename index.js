d6ea39676a853a6aa3351543bec67dd67c3c104d
f.munoz.munoz <f.munoz.munoz@accenture.com>
2021-03-02 19:22:06 +0100

    update buttons

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
+import escape from "./src/js/escape.js";
 
+const DOMContentLoaded = () => {
+  document.addEventListener("DOMContentLoaded", () => {
+    // Code highlight
+    document.querySelectorAll("pre code").forEach((block) => {
+      escape.html(block, true);
+      hljs.highlightBlock(block);
+    });
+  });
+};
+
+export default DOMContentLoaded;
